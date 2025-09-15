e244b6439c0460eaa3ed1fb5dc087ac855facb24
DonaldKisaka <kisakadonald1@gmail.com>
2025-09-15 18:12:43 +0300

    Repositories

---
+++ donaldkisaka/src/main/java/com/blog/donaldkisaka/model/CommentRepository.java
@@ -0,0 +1,7 @@
+package com.blog.donaldkisaka.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+}
---
+++ donaldkisaka/src/main/java/com/blog/donaldkisaka/model/PostRepository.java
@@ -0,0 +1,6 @@
+package com.blog.donaldkisaka.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
