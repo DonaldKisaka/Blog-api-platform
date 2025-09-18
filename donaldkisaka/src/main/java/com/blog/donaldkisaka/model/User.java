1c8b9914f130496f5a102195ee585783364916d7
DonaldKisaka <kisakadonald1@gmail.com>
2025-09-18 10:50:06 +0300

    User model

--- donaldkisaka/src/main/java/com/blog/donaldkisaka/model/User.java
+++ donaldkisaka/src/main/java/com/blog/donaldkisaka/model/User.java
@@ -43,6 +43,8 @@ public class User implements UserDetails {
     private LocalDateTime createdAt;
 
 
+
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(new SimpleGrantedAuthority(role.name()));
