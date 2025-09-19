71a8e6aa8ba025df4069323a30b90bd378083dc0
DonaldKisaka <kisakadonald1@gmail.com>
2025-09-19 15:50:01 +0300

    CreatePost

---
+++ donaldkisaka/src/main/java/com/blog/donaldkisaka/dto/CreatePost.java
@@ -0,0 +1,15 @@
+package com.blog.donaldkisaka.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class CreatePost {
+    @NotBlank(message = "Title is required")
+    private String title;
+
+    @NotBlank(message = "Content is required")
+    private String content;
+
+    private String author;
+}
