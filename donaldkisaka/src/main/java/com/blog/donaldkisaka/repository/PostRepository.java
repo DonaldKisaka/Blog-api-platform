6d4a3db35f915ef76902cd5fc7ccab281b40a9cf
DonaldKisaka <kisakadonald1@gmail.com>
2025-09-19 15:48:58 +0300

    PostRepository

--- donaldkisaka/src/main/java/com/blog/donaldkisaka/repository/PostRepository.java
+++ donaldkisaka/src/main/java/com/blog/donaldkisaka/repository/PostRepository.java
@@ -1,8 +1,20 @@
 package com.blog.donaldkisaka.repository;
 
 import com.blog.donaldkisaka.model.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+    Page<Post> findAllByDeletedAtIsNull(Pageable pageable);
+
+    Optional<Post> findByIdAndDeletedAtIsNull(Long postId);
+
+    Page<Post> findAllByPublishedAndDeletedAtIsNull(Boolean published, Pageable pageable);
+
+    Page<Post> findAllByPublishedAtBeforeAndDeletedAtIsNull(LocalDateTime publishedAt, Pageable pageable);
 }
