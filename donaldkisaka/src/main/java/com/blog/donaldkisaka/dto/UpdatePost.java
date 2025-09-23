6e935c05124f7bff294d259aaae6ef20699edfff
DonaldKisaka <kisakadonald1@gmail.com>
2025-09-23 08:43:29 +0300

    UpdatePost

--- donaldkisaka/src/main/java/com/blog/donaldkisaka/dto/UpdatePost.java
+++ donaldkisaka/src/main/java/com/blog/donaldkisaka/dto/UpdatePost.java
@@ -1,14 +1,19 @@
 package com.blog.donaldkisaka.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UpdatePost {
     @NotBlank(message = "Title is required")
+    @Size(max = 200)
     private String title;
 
     @NotBlank(message = "Content is required")
+    @Size(max = 10000)
     private String content;
+
+    private Boolean published;
 
 }
