4137184a925d4124738505c359d2c31826c502ca
DonaldKisaka <kisakadonald1@gmail.com>
2025-09-23 09:49:15 +0300

    PostResponse

--- donaldkisaka/src/main/java/com/blog/donaldkisaka/response/PostResponse.java
+++ donaldkisaka/src/main/java/com/blog/donaldkisaka/response/PostResponse.java
@@ -1,14 +1,12 @@
 package com.blog.donaldkisaka.response;
 
-import com.blog.donaldkisaka.model.User;
-
 import java.time.LocalDateTime;
 
 public record PostResponse(
         Long id,
         String title,
         String content,
-        User author,
+        AuthorInfo author,
         Boolean published,
         LocalDateTime createdAt,
         LocalDateTime updatedAt
